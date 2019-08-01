2d025e31e592fe361a43b17c915a13643f88c67f
Nithish136 <44423123+Nithish136@users.noreply.github.com>
2019-08-01 19:51:02 +0530

    Update change_dp.py

--- change_dp.py
+++ change_dp.py
@@ -1,8 +1,12 @@
 #Uses python3
+''' 
+It decreases the complexity of changing problem by simply storing intermediate results and using them in the middile.
+'''
 import sys
 from decimal import Decimal
 def get_change(m):
-    #write your code here
+    #This returns the minimum number of coins required to convert m using [1,3,4] coins 
+    #to change 5 our function returns 2 [1,4] 
     if m == 1:
         return 1
     change = [0]
@@ -15,6 +19,7 @@ def get_change(m):
         elif i%4 ==0:
             change[i] = i//4     
         else:
+            # It computes the minimum number of coins required for the ith number can be computed using the past change 
            change[i] = min(change[i-1]+1 ,change[i-2]+1, change[i-3] + 1, change[i-4]+1 )          
     return change[m-1]
 
